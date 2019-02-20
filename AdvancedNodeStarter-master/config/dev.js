bc7b9ea407c037b435843cbc39f1ca555098da3e
Cody Taft <cody.taft@gmail.com>
2019-02-20 14:34:45 -0700

    Setup MongoDB

--- AdvancedNodeStarter-master/config/dev.js
+++ AdvancedNodeStarter-master/config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb://cody_taft:Wkpkss-1983@cluster0-shard-00-00-jgpjh.mongodb.net:27017,cluster0-shard-00-01-jgpjh.mongodb.net:27017,cluster0-shard-00-02-jgpjh.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
   cookieKey: '123123123'
 };
