a26961bf58d22c68a5bcf62e1965cd52f2dcf5af
Cody Taft <cody.taft@gmail.com>
2019-02-19 21:46:16 -0700

    Add express server

---
+++ index.js
@@ -0,0 +1,27 @@
+const cluster = require('cluster');
+
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Casue index.js to be executed *again* but
+  // in child mode
+  cluster.fork();
+} else {
+  // Im a child, im going to act like a server
+  // and do nothing else
+  const express = require('express');
+
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.listen(3000);
+}
