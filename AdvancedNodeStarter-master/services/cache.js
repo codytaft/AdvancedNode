6e8d2937b5992d70fb6dcd2468bf6c62e367a045
Cody Taft <cody.taft@gmail.com>
2019-02-21 16:01:29 -0700

    Check if cacheValue is array

--- AdvancedNodeStarter-master/services/cache.js
+++ AdvancedNodeStarter-master/services/cache.js
@@ -2,24 +2,37 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
 
-const redisUrl = 'redisL//128.0.0.1:6379';
+const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 
 client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log('Im about to run a query');
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  console.log(this.getQuery());
-  console.log(this.mongooseCollection.name);
+  // See if we have a value for 'key in redis
 
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
+  const cacheValue = await client.get(key);
 
-  console.log(JSON.stringify(key));
+  //If we do, return that
 
-  return exec.apply(this, arguments);
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  //Otherwise, issue the query and store the reuslt in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+  return result;
 };
